e4a70c03e60a2d0ee561efb9950eeac1c4bec43c
Isaiah-B <90220160+Isaiah-B@users.noreply.github.com>
2023-08-27 16:20:25 -0500

    Improved link validation

--- app/src/constants.tsx
+++ app/src/constants.tsx
@@ -23,98 +23,98 @@ export const LINK_SITES: StringIndex = {
     placeholderUrl: "https://www.github.com/johnappleseed",
     logo: GitHubLogo,
     color: "hsl(0, 0%, 10%)",
-    validation: /https:\/\/www\.github\.com\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?github\.com\/[\w\-\/]*/
   },
   youtube: {
     name: "YouTube",
     placeholderUrl: "https://www.youtube.com/johnappleseed",
     logo: YouTubeLogo,
     color: "hsl(0, 84%, 58%)",
-    validation: /https:\/\/www\.youtube\.com\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?youtube\.com\/.*/
   },
   twitter: {
     name: "Twitter",
     placeholderUrl: "https://www.twitter.com/johnappleseed",
     logo: TwitterLogo,
     color: "hsl(198, 79%, 59%)",
-    validation: /https:\/\/www\.twitter\.com\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?twitter\.com\/[\w\-\/]*/
   },
   linkedin: {
     name: "LinkedIn",
     placeholderUrl: "https://www.linkedin.com/johnappleseed",
     logo: LinkedInLogo,
     color: "hsl(223, 100%, 59%)",
-    validation: /https:\/\/www\.linkedin\.com\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?linkedin\.com\/[\w\-\/]*/
   },
   facebook: {
     name: "Facebook",
     placeholderUrl: "https://www.facebook.com/johnappleseed",
     logo: FacebookLogo,
     color: "hsl(227, 66%, 41%)",
-    validation: /https:\/\/www\.facebook\.com\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?facebook\.com\/[\w\-\/]*/
   },
   frontendmentor: {
     name: "Frontend Mentor",
     placeholderUrl: "https://www.frontendmentor.io/johnappleseed",
     logo: FrontendMentorLogo,
     color: "hsl(0, 0%, 100%)",
-    validation: /https:\/\/www\.frontendmentor\.io\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?frontendmentor\.io\/[\w\-\/]*/
   },
   twitch: {
     name: "Twitch",
     placeholderUrl: "https://www.twitch.tv/johnappleseed",
     logo: TwitchLogo,
     color: "hsl(313, 84%, 59%)",
-    validation: /https:\/\/www\.twitch\.tv\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?twitch\.tv\/[\w\-\/]*/
   },
   devto: {
     name: "Dev.to",
     placeholderUrl: "https://dev.to/johnappleseed",
     logo: DevtoLogo,
     color: "hsl(0, 0%, 20%)",
-    validation: /https:\/\/www\.dev\.to\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?dev\.to\/[\w\-\/]*/
   },
   codewars: {
     name: "Codewars",
     placeholderUrl: "https://www.codewars.com/users/johnappleseed",
     logo: CodewarsLogo,
     color: "hsl(331, 68%, 32%)",
-    validation: /https:\/\/www\.codewars\.com\/users\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?codewars\.com\/users\/[\w\-\/]*/
   },
   codepen: {
     name: "Codepen",
     placeholderUrl: "https://codepen.io/johnappleseed",
     logo: CodepenLogo,
     color: "hsl(145, 68%, 32%)",
-    validation: /https:\/\/www\.codepen\.io\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?codepen\.io\/[\w\-\/]*/
   },
   freecodecamp: {
     name: "freeCodeCamp",
     placeholderUrl: "https://www.freecodecamp.org/johnappleseed",
     logo: FreeCodeCampLogo,
     color: "hsl(252, 50%, 27%)",
-    validation: /https:\/\/www\.freecodecamp\.org\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?freecodecamp\.org\/[\w\-\/]*/
   },
   gitlab: {
     name: "GitLab",
     placeholderUrl: "https://gitlab.com/johnappleseed",
     logo: GitLabLogo,
     color: "hsl(11, 83%, 53%)",
-    validation: /https:\/\/www\.gitlab\.com\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?gitlab\.com\/[\w\-\/]*/
   },
   hashnode: {
     name: "Hashnode",
     placeholderUrl: "https://hashnode.com/@johnappleseed",
     logo: HashNodeLogo,
     color: "hsl(227, 98%, 42%)",
-    validation: /https:\/\/www\.hashnode\.com\/@[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?hashnode\.com\/@[\w\-\/]*/
   },
   stackoverflow: {
     name: "Stack Overflow",
     placeholderUrl: "https://stackoverflow.com/users/1234/johnappleseed",
     logo: StackOverflowLogo,
     color: "hsl(29, 100%, 46%)",
-    validation: /https:\/\/www\.stackoverflow\.com\/users\/\d+\/[\w\-]*/
+    validation: /(https:\/\/)?(www\.)?stackoverflow\.com\/users\/\d+\/[\w\-\/]*/
   },
 };
 
